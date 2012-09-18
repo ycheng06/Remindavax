b26e2f0d285e7f2589036b42b1595fd0be7738a7
Jason Cheng <yannru.cheng@gmail.com>
2012-09-17 23:43:55 -0400

    frequency input box added to new drug page

--- Remindavax/app/helpers/drugs_helper.rb
+++ Remindavax/app/helpers/drugs_helper.rb
@@ -1,6 +1,7 @@
 module DrugsHelper
   def setup_drug(drug)
     drug.dosage ||= Dosage.new
+    drug.frequency ||= Frequency.new
     drug
   end
 end
--- Remindavax/app/models/frequency.rb
+++ Remindavax/app/models/frequency.rb
@@ -1,4 +1,7 @@
 class Frequency < ActiveRecord::Base
   attr_accessible :days_per_week, :times_per_day, :weeks_per_month
   belongs_to :drug
+  
+  validates :days_per_week, :times_per_day, :weeks_per_month, 
+    :numericality => true
 end
